6f67110936ff67591fc639d415a69b90796e1221
Santiago <gato.jijen01@gmail.com>
2025-06-17 23:37:18 -0500

    a16.5

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+// .eslintrc.js
+module.exports = {
+    // ...
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+    },
+  };
+  
