a9ecd4fd9d384ab8df82c8d2622610ebbda11aed
Santiago <gato.jijen01@gmail.com>
2025-06-17 23:53:54 -0500

    a17.5

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  ignorePatterns: [
+    "node_modules",
+    "dist",
+    ".eslintrc.js",
+    "src/app/dashboard/Clic-Up/page.tsx",
+    "src/app/dashboard/Clic-Up/Usuarios/page.tsx",
+    "src/app/dashboard/Clic-Up/Chats/page.tsx",
+    "src/app/dashboard/Clic-Up/Avisos/page.tsx",
+    "src/components/SearchAvisos.tsx",
+    "src/components/App1Avisos.tsx",
+    "src/components/Loading.tsx",
+    "src/components/HomeWelcome.tsx",
+    "src/components/HomeNav.tsx",
+    "src/components/HomeHeader.tsx",
+    "src/components/HomeGradient.tsx",
+    "src/components/**.tsx",
+  ],
+};
